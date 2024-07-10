b9fad5bea29867cb65cdd2b1b8e035e3882f5c85
aliaksandr <aliaksandr_fomin@list.ru>
2024-07-10 18:53:17 +0300

    Fibanachi

--- src/main/java/Zoo.java
+++ src/main/java/Zoo.java
@@ -3,52 +3,31 @@ import java.util.*;
 
 public class Zoo {
 
-    public static void main (String[]arg){
-
-        Animal animal = Animal.BIRD;
-        for (Animal element: Animal.values()){System.out.println(element);}
-
-//
-//       System.out.println(animal.getName());
-//       System.out.println(animal.getIdAnimal());
+    public static void main(String[] arg) {
 
 
+        int num0 = 0;
+        int num1 = 1;
+        int num2;
 
 
+        System.out.print(" "+ num0 );
+        System.out.print(" "+ num1+" "  );
+        for(int i = 3; i <= 10; i++){
+            num2 = num0 + num1;
 
 
-        List<String> bird = new ArrayList<>();
-        bird.add("eagle");
-        bird.add("crow");
-        bird.add("sova");
+            System.out.print(num2 + " ");
 
-        System.out.println("In our zoo you can see " + bird  );
 
-        Set<String> fish = new HashSet<>();
-        fish.add("salmon");
-        fish.add("som");
-        fish.add("shark");
-
-        System.out.println(fish);
-
-        Map<String,String> beast = new LinkedHashMap<>();
-        beast.put("bear2","grizly");
-        beast.put("bear2","russian");
-        beast.put("wolf1","grey");
-        beast.put("wolf2","arctic");
-
-        System.out.println(beast);
+            num0 = num1;
+            num1 = num2;
+        }
 
 
 
-        animal.inforationFish();
-        animal.inforationBird();
-        animal.inforationBeast();
-
-
+        }
     }
-
-}
 
 
 
