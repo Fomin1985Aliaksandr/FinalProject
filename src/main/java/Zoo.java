dcb43d9c45addecf6257e8ef81dc106f200bd1a2
aliaksandr <aliaksandr_fomin@list.ru>
2024-07-10 18:53:17 +0300

    Enums+collection

--- src/main/java/Zoo.java
+++ src/main/java/Zoo.java
@@ -3,52 +3,26 @@ import java.util.*;
 
 public class Zoo {
 
-    public static void main (String[]arg){
-
-        Animal animal = Animal.BIRD;
-        for (Animal element: Animal.values()){System.out.println(element);}
-
-//
-//       System.out.println(animal.getName());
-//       System.out.println(animal.getIdAnimal());
+    public static void main(String[] arg) {
 
 
 
+        int num0 = 1;
+        int num1 = 1;
+        int num2;
 
 
-
-        List<String> bird = new ArrayList<>();
-        bird.add("eagle");
-        bird.add("crow");
-        bird.add("sova");
-
-        System.out.println("In our zoo you can see " + bird  );
-
-        Set<String> fish = new HashSet<>();
-        fish.add("salmon");
-        fish.add("som");
-        fish.add("shark");
-
-        System.out.println(fish);
-
-        Map<String,String> beast = new LinkedHashMap<>();
-        beast.put("bear2","grizly");
-        beast.put("bear2","russian");
-        beast.put("wolf1","grey");
-        beast.put("wolf2","arctic");
-
-        System.out.println(beast);
+       System.out.print(" "+ num0 );
+        System.out.print(" "+ num1+" "  );
+        for(int i = 3; i <= 10; i++){
+            num2 = num0 + num1;
 
 
-
-        animal.inforationFish();
-        animal.inforationBird();
-        animal.inforationBeast();
+            System.out.print(num2 + " ");
 
 
+        }
     }
 
 }
-
-
 
